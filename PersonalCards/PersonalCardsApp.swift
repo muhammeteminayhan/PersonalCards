dcc20338fed79f532d30c52fe9140b84d2efd278
Muhammet Emin Ayhan <aemin8343@gmail.com>
2024-10-30 18:58:41 +0300

    Initial commit

---
+++ PersonalCards/PersonalCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalCardsApp.swift
+//  PersonalCards
+//
+//  Created by Muhammet Emin Ayhan on 30.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PersonalCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
